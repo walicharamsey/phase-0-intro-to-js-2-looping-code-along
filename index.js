331b58942d2b9eb9f25b2e122504bf5a7d16cf02
walicharamsey <ramsey@Cullen-PC.localdomain>
2023-10-09 11:01:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
